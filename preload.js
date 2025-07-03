12f8cab6c4a55a1b2b51a0f7ab1a66f910f9946c
kouta15-zm <69737623+kouta15-zm@users.noreply.github.com>
2025-07-03 15:20:06 -0400

    Agregar electron-packager como dependencia de desarrollo y actualizar package-lock.json con nuevas dependencias relacionadas con Electron.

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
+ 
 contextBridge.exposeInMainWorld('electronAPI', {
   selectVideosFolder: () => ipcRenderer.invoke('select-videos-folder'),
   getVideosFolder: () => ipcRenderer.invoke('get-videos-folder')
